8b807dc1a37274d960162f6b58073faa112fb537
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-08-03 17:06:51 +1200

    add join tables

--- server/db/comments.ts
+++ server/db/comments.ts
@@ -2,16 +2,21 @@ import { Comments } from '../../models/comments'
 import db from './connection'
 
 export async function getAllComments() {
-  const comment = await db('comments')
-    .join('users', 'comments.user_id', 'user.id')
-    .join('replies', 'comments.id', 'replies.parent_id')
+  const commentsWithReplies = await db('comments')
+    .leftJoin('users as comment_user', 'comments.user_id', 'comment_user.id')
+    .leftJoin('replies', 'comments.id', 'replies.parent_id')
+    .leftJoin('users as reply_user', 'replies.user_id', 'reply_user.id')
     .select(
-      'comments.id',
-      'comments.user_id',
-      'users.user_name',
-      'users.img_url',
-      'comments.body',
-      'comments.created_at',
+      'comments.id as comment_id',
+      'comments.body  as comment_body',
+      'comments.created_at as comment_created_at',
+      'users.user_name as comment_user_name',
+      'users.img_url as comment_img_url',
+      'replies.id as reply_id',
+      'replies.body as reply_body',
+      'replies.created_at as reply_created_at',
+      'reply_user.user_name as reply-user_name',
+      'reply_user.img_url as reply_user_img_url',
     )
-  return comment as Comments[]
+  return commentsWithReplies as Comments[]
 }
